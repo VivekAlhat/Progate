22c10da530f1e352a15d4e5166f902e6f24fb11a
Vivek Alhat <vivekalhat14@outlook.com>
2019-10-23 19:04:44 +0530

    Create Study 1

--- Progate JavaScript/Horoscope.js
+++ Progate JavaScript/Horoscope.js
@@ -1,7 +1,7 @@
 const number = Date.now()
 let ans = number % 3;
 const str = "Your luck for today is: ";
-
+// console.log(number)
 if(ans === 0){
   console.log(str + "Great");
 } else if(ans == 1){
